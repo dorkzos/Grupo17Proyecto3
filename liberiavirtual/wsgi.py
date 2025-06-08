1f02a4bf72a52682c3bdc8293933ac51effa42da
dorkzos <dpmenendezlimo@gmail.com>
2025-06-08 17:23:33 -0400

    Corregido proyecto

--- liberiavirtual/wsgi.py
+++ liberiavirtual/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for libreriaonline project.
+WSGI config for liberiavirtual project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'libreriaonline.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liberiavirtual.settings')
 
 application = get_wsgi_application()
