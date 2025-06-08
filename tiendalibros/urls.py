bf8834bb557c11de29c7a52003f45f8dbe19debf
dorkzos <dpmenendezlimo@gmail.com>
2025-06-08 19:46:07 -0400

    Implementando primera version login/registrarse

--- tiendalibros/urls.py
+++ tiendalibros/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from .views import inicio, usuario_logout
+
+urlpatterns = [
+    path('', inicio, name='inicio'),
+    path('logout/', usuario_logout, name='usuario_logout'),
+]
--- tiendalibros/views.py
+++ tiendalibros/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def inicio(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('inicio')  # Redirige a la misma vista para mostrar el mensaje de bienvenida
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'inicio.html', {'form': form})
+
+def usuario_logout(request):
+    logout(request)
+    return redirect('inicio')
