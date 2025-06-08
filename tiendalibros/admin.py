11ee193d9b492ed5785b06f39db9967ee71141c9
dorkzos <dpmenendezlimo@gmail.com>
2025-06-08 18:35:59 -0400

    Agregando models al admin DJANGO

--- tiendalibros/admin.py
+++ tiendalibros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Autor, Libro, Pedido, DetallePedido, Historial
 
-# Register your models here.
+admin.site.register(Autor)
+admin.site.register(Libro)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
+admin.site.register(Historial)
