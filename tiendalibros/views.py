f689cd86a66c2e6df7bcbc02e0c54e6563a22199
dorkzos <dpmenendezlimo@gmail.com>
2025-06-08 20:26:36 -0400

    cambios en pagina inicio

--- tiendalibros/views.py
+++ tiendalibros/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def inicio(request):
     if request.user.is_authenticated:
-        # Usuario logueado
         return render(request, 'inicio_logueado.html', {
             'usuario': request.user
         })
     else:
-        # Usuario no logueado, formulario de login
         if request.method == 'POST':
             form = AuthenticationForm(request, data=request.POST)
             if form.is_valid():
@@ -22,6 +20,17 @@ def inicio(request):
         else:
             form = AuthenticationForm()
         return render(request, 'inicio.html', {'form': form})
+
+def registrarse(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('inicio')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registrarse.html', {'form': form})
 
 def usuario_logout(request):
     logout(request)
