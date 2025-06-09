66a5f3c6abf07391627cc8bf8c976a915008e6f7
dorkzos <dpmenendezlimo@gmail.com>
2025-06-08 20:09:36 -0400

    Se simplificó login

--- tiendalibros/urls.py
+++ tiendalibros/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import inicio, usuario_logout
 
 urlpatterns = [
-    path('', inicio, name='inicio'),
+    path('', inicio, name='inicio'),              
+    path('inicio/', inicio, name='inicio_url'),   
     path('logout/', usuario_logout, name='usuario_logout'),
 ]
--- tiendalibros/views.py
+++ tiendalibros/views.py
@@ -3,19 +3,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 
 def inicio(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('inicio')  # Redirige a la misma vista para mostrar el mensaje de bienvenida
+    if request.user.is_authenticated:
+        # Usuario logueado
+        return render(request, 'inicio_logueado.html', {
+            'usuario': request.user
+        })
     else:
-        form = AuthenticationForm()
-
-    return render(request, 'inicio.html', {'form': form})
+        # Usuario no logueado, formulario de login
+        if request.method == 'POST':
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('inicio')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'inicio.html', {'form': form})
 
 def usuario_logout(request):
     logout(request)
